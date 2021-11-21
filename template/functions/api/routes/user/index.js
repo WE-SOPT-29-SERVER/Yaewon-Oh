672d19663d8473b68e0de7e9aa64d6954556a9e2
yaeoni <tjghwnd103@gmail.com>
2021-11-21 14:25:13 +0900

    [FEAT] Jwt 생성 / verity 추가

--- template/functions/api/routes/user/index.js
+++ template/functions/api/routes/user/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
 router.get('/list', require('./userListGET'));
-router.get('/:userId', require('./userGET'));
+router.get('/:userId', checkUser, require('./userGET'));
 router.put('/:userId', require('./userPUT'));
 router.delete('/:userId', require('./userDELETE'));
 
